3ee188141e0d64f0b1d71e5b472827814a3174d3
marra abdallah <abdallah4marra@gmail.com>
2023-09-22 16:36:17 +0100

    updated

--- day26/app.js
+++ day26/app.js
@@ -1,110 +1,150 @@
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser'); // Add cookie-parser middleware
-const csurf = require('csrf');
-const { body, validationResult } = require('express-validator');
+// const csurf = require('csrf');
+const { body, validationResult, Result } = require('express-validator');
 const path = require('path');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const xss = require('xss');
 const bcrypt = require('bcrypt')
 const app = express();
+const fs = require('fs');
+
+
 const secretKey = '1c81da0609e0cbc608e98cd0c36d63d4a24483f4a583bb17cf8710a3aa8c6045';
 
-app.use(express.urlencoded({ extended: false }));
 app.use(session({ secret: secretKey, resave: false, saveUninitialized: false }));
 app.use(cookieParser()); // Use cookie-parser middleware
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
-const users = [
-  { id: 1, username: 'user1', password: 'password1', role: 'user'  },
-  { id: 2, username: 'admin', password: 'admin123', role: 'admin' },
-];
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index', { csrfToken: 'req.csrfToken()' });
+app.get('/regester', (req, res) => {
+  res.render('regester');
 });
 
-app.post('/login', [
+
+
+
+const users = require('./data/usersInfos.json');
+  app.post('/regester',
+[
   body('username').notEmpty().trim().escape(),
+  body('email').isEmail().normalizeEmail(),
+  body('phone').isNumeric().trim(),
   body('password').isLength({ min: 6 })
-], (req, res) => {
+],
+ (req, res) => {
   const err = validationResult(req);
   if (!err.isEmpty()) {
     res.redirect('/');
   }
-  const { username, password } = req.body;
-    const securedData= {
-      username: xss(username),
-      password: xss(password)
+  const { username, email, phone, password } = req.body;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.error(err);
+    } else {
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const securedData= {
+          username: xss(username),
+          email: xss(email),
+          phone:xss(phone),
+          password: xss(hash)
+          }
+        users.push(securedData)
+        
+        fs.writeFile('./data/usersInfos.json', JSON.stringify(users, null, 2), (err) =>{
+            if(err){
+              throw err
+            }
+        });
+        }
+      });
     }
-  const user = users.find((u) => u.username === username && u.password === password);
-  if (user) {
-    const token = jwt.sign({ userId: user.id, userRole: user.role }, secretKey);
-    console.log({token})
+    res.redirect('/login');
+  });
 
-    //hash the password
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) {
-        console.error(err);
-      } else {
-        bcrypt.hash(user.password, salt, (err, hash) => {
+});
+app.get('/login', (req, res)=>{
+  res.render('index')
+})
+
+app.post('/login', (req, res)=>{
+  const { username, password } = req.body;
+  // console.log(username)
+  fs.readFile('./data/usersInfos.json', 'utf8', (err, data)=>{
+    if(err){
+      throw err
+    }
+      const users = JSON.parse(data);
+      const user = users.find((u) => u.username === username);
+      if (user) {
+        bcrypt.compare(password, user.password, (err, result) => {
+          console.log(password)
+          console.log(user.password)
           if (err) {
             console.error(err);
+            return res.status(500).send('Internal Server Error');
+          }
+        
+          if (result) {
+            console.log(result)
+            const token = jwt.sign({ userId: user.phone }, secretKey);
+            res.cookie('authToken', token);
+            return res.redirect('/dashboard');
           } else {
-            console.log(hash);
-            users.push({hashed:hash})
-            console.log(users);
+            return res.status(404).send(' password is incorrect');
           }
         });
-      }
-    });
-    
-    // Set a cookie to indicate authentication
-    res.cookie('authToken', token);
-    
-    req.session.isAuthenticated = true;
-    res.redirect('/dashboard');
-  } else {
-    res.status(404).send({ error: 'the username or password are not correct' });
-  }
-});
+        
+      }else{
+        return res.status(404).send(' username is incorrect');
+      } 
+  });
+})
+
+// app.get('/dashboard', (req, res)=>{
+//   res.render('dashboard')
+// })
 
 app.get('/dashboard', (req, res) => {
-  // Check if the 'authToken' cookie exists
-if (req.cookies.authToken) {
+  // Check if the user is authenticated here using the JWT
   const token = req.cookies.authToken;
-    // console.log(req.cookies.authToken)
-  // Verify and decode the JWT token
+  
+  if (!token) {
+    return res.redirect('/login');
+  }
+
   jwt.verify(token, secretKey, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ message: 'Access denied' });
+      return res.redirect('/login');
     }
-    // if User is authenticated ill use user data via decoded`
-    const { userId, userRole } = decoded;
-    if (userRole === 'admin') {
-      res.render('dashboard');
-    } else {
-      res.status(403).send({ error: 'You are not allowed to access this page; it is only for admins' });
-    }
+
+    // Here, you can render the dashboard for authenticated users
+    res.render('dashboard');
   });
-} else {
-  res.status(401).json({ message: 'Unauthorized' });
-}
+});
+
+
 
 
 // Logout route
 app.get('/logout', (req, res) => {
   // Clear the 'authToken' cookie
   res.clearCookie('authToken');
-  res.redirect('/');
+  res.redirect('/login');
 });
 
-});
+// 
 
 
 
